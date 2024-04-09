61c673db88f42cf21a4b124e49260b0163eced82
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-04-09 10:11:53 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,49 @@
 import os
 
-print("""Sabor MDA (MANTIOCA DO AMOR)
+restaurantes = ['pizza','xp']
+
+def exibir_nome_do_programa():
+    print("""Sabor MDA (MANTIOCA DO AMOR)
       """)
 
-print("1)cadastrar restaurante")
-print("2)listar restaurante")
-print("3)ativar restaurante")
-print("4)sair")
+def exibir_opcoes():
+ print("1)cadastrar restaurante")
+ print("2)listar restaurante")
+ print("3)ativar restaurante")
+ print("4)sair")
+
+def finaliza_app():
+    exibir_subtitulo('finalizar app')
+
+def voltar_ao_menu_principal():
+    input('\n Digite a tecla "enter" para voltar ao menu principal')
+    main()
 
 opcao_escolhida = int(input("Escolha uma opção"))
 
-def finaliza_app():
+def exibir_subtitulo(texto):
     os.system('clear')
-    print("encerrando o programa\n")
+    print(texto)
+    print()
 
-if opcao_escolhida == 1:
-    print('cadastrar retaurante')
-elif opcao_escolhida == 2:
-    print('listar restaurantes') 
-elif opcao_escolhida == 3:
-    print('Ativar restaurantes')
-else:
-    finaliza_app()
+def escolher_opcao():
 
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurantes')
+    else:
+        finaliza_app()
 
+def main():        #meu main é o rico
+    os.system('clear')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+    if __name__ == '__main__':
+        main()
+
+        
